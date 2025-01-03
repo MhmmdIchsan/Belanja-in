c63fd1d6020709c35701561555c95ed0d56b6239
MhmmdIchsan <mhichsan21@mhs.usk.ac.id>
2025-01-03 16:04:00 +0700

    feat: refactor App component and remove unused CSS files for cleaner structure

--- ecommerce-frontend/src/main.jsx
+++ ecommerce-frontend/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import 'https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
