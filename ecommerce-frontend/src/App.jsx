ca764196281d943f4520ef43af8ff9ed0b870364
MhmmdIchsan <mhichsan21@mhs.usk.ac.id>
2025-01-07 16:19:00 +0700

    feat: update App component to include AboutUsPage and Footer components

--- ecommerce-frontend/src/App.jsx
+++ ecommerce-frontend/src/App.jsx
@@ -5,9 +5,13 @@ import { CartProvider } from "./context/CartContext";
 import { AuthProvider } from "./context/AuthContext";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import ProductDetailPage from "./pages/ProductDetailPage";
 import CartPage from "./pages/CartPage";
+import ProfilePage from "./pages/ProfilePage";
+import AboutUsPage from "./pages/AboutUsPage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -18,9 +22,13 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/cart" element={<CartPage />} />
+            <Route path="/product/:id" element={<ProductDetailPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/about-us" element={<AboutUsPage />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
           </Routes>
+          <Footer />
         </CartProvider>
       </AuthProvider>
     </Router>
