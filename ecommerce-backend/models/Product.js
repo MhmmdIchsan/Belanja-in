66a396abc836c7840042e956aef9216d45995e47
MhmmdIchsan <mhichsan21@mhs.usk.ac.id>
2025-01-08 16:17:00 +0700

    feat: add product model with schema for name, price, and rating

---
+++ ecommerce-backend/models/Product.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: String,
+  description: String,
+  category: String,
+  price: {
+    type: Number,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+}, {
+  timestamps: true,
+})
+
+module.exports = mongoose.model('Product', productSchema)
