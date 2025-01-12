a63831f9ff5696d8d4c8352f2682dab45071110d
MhmmdIchsan <mhichsan21@mhs.usk.ac.id>
2025-01-12 16:19:00 +0700

    refactor: restructure cart schema to include variation details and improve item definition

--- ecommerce-backend/models/Cart.js
+++ ecommerce-backend/models/Cart.js
@@ -1,27 +1,46 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const cartSchema = new mongoose.Schema({
-  user: {
+const cartItemSchema = new mongoose.Schema({
+  product: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
+    ref: "Product",
     required: true,
   },
-  items: [
-    {
-      product: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Product',
-        required: true,
-      },
-      quantity: {
-        type: Number,
-        default: 1,
-        min: 1,
-      },
-    }
-  ]
-}, {
-  timestamps: true,
-})
+  variation: {
+    color: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String, // URL gambar variasi
+    },
+  },
+  quantity: {
+    type: Number,
+    default: 1,
+    min: 1,
+  },
+});
 
-module.exports = mongoose.model('Cart', cartSchema)
+const cartSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    items: [cartItemSchema],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Cart", cartSchema);
